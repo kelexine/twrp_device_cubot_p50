fe19093a7104b0ea30020f692fb5440344d0133d
kelexine <frankiekelechi@gmail.com>
2024-03-12 12:48:12 +0100

    initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_P50.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_P50-user \
+    twrp_P50-userdebug \
+    twrp_P50-eng
---
+++ README.md
@@ -0,0 +1,3 @@
+# Twrp Recovery Tree for Cubot P50
+
+ Still a work in progress 
