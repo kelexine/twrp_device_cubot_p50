b2995b2238e59256111e131dba98f6f72fcaec9d
Franklin Kelechi <70432347+kelexine@users.noreply.github.com>
2025-12-26 23:30:49 +0100

    Update twrp_marlon.mk

--- twrp_marlon.mk
+++ twrp_marlon.mk
@@ -5,9 +5,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from marlon device
 $(call inherit-product, device/cubot/marlon/device.mk)
