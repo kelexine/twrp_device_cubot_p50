61833cbf53ca7c470a2912501a0290ced0659e9c
Franklin Kelechi <70432347+kelexine@users.noreply.github.com>
2025-12-26 23:21:50 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,7 +9,8 @@ DEVICE_PATH := device/cubot/marlon
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
-TARGET_USES_64_BIT_BINDER := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Architecture
 TARGET_ARCH := arm64
